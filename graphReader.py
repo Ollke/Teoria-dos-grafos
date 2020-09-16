54a7066b35025f80e9884b77c834bb1aad378ef4
paulo <paulo_gust@hotmail.com>
2020-09-16 13:45:30 -0300

    update on graphReader and add graph example

---
+++ graphReader.py
@@ -0,0 +1,21 @@
+data = open("graph.txt","r")
+
+graph = {}
+
+for i in data:
+    j = i.split()
+    aux = []
+
+    for r in range(1,len(j)):
+        aux.append(j[r])
+
+    graph[j[0]] = aux
+
+data.close()
+
+for i in graph:
+    print(f"{i}:",end="")
+
+    for j in graph[i]:
+        print(f" {j}",end="")
+    print("")
